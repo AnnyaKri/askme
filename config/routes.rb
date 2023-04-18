f616bbc9f49dbf7a0f74e067340d6c29ef9bc5f6
AnnyaKri <sobakajustdoit@gmail.com>
2023-04-18 10:03:18 +0300

    changes in css, controller, views, routes, db

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: "questions#index"
-
   resources :questions do
     member do
       put :hide
     end
   end
+
+  resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
 end
