339451df602e8c3e68f2a60ba0bddde6d59e307d
Ann <125645078+AnnyaKri@users.noreply.github.com>
2023-04-19 10:08:10 +0300

    Update questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
   def create
     question = Question.create(question_params)
-    redirect_to question_path(question), notice: "Новый вопрос создан!"
+    redirect_to questions_path(question), notice: "Новый вопрос создан!"
   end
 
   def update
