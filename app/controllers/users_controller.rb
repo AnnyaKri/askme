d6bdf68cc61035d65ce7fcbfddbae61ea4a9a8f1
AnnyaKri <sobakajustdoit@gmail.com>
2023-04-19 08:49:45 +0300

    add password and Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,45 @@ class UsersController < ApplicationController
     session[:current_time] = Time.now
     @user = User.new
   end
+
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Вы успешно зарегистрировались!"
+    else
+      flash.now[:alert] = "Вы неправильно заполнили поля формы регистрации"
+      render :new
+    end
+  end
 
-    redirect_to root_path, notice: "Вы успешно зарегистрировались!"
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Данные пользователя сохранены!"
+    else
+      flash.now[:alert] = "При попытке сохранить пользователя произошли ошибки"
+      render :edit
+    end
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+    redirect_to root_path, notice: ' Пользователь удален'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
   end
 end
