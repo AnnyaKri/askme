4b47f23be62938f1ea5ea208045bcaeba18d334d
AnnyaKri <sobakajustdoit@gmail.com>
2023-04-25 12:42:31 +0300

    added automatic authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,13 @@ class UsersController < ApplicationController
   before_action :authorize_user, only: %i[edit update destroy]
 
   def new
-    session[:current_time] = Time.now
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Вы успешно зарегистрировались!"
     else
       flash.now[:alert] = "Вы неправильно заполнили поля формы регистрации"
@@ -22,7 +21,6 @@ class UsersController < ApplicationController
   end
 
   def update
-
     if @user.update(user_params)
       redirect_to root_path, notice: "Данные пользователя сохранены!"
     else
@@ -33,7 +31,6 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-
     session.delete(:user_id)
     redirect_to root_path, notice: ' Пользователь удален'
   end
