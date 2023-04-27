7f3d6b6c14e3267014ea62d8a18c9150427fe7fe
AnnyaKri <sobakajustdoit@gmail.com>
2023-04-27 13:57:00 +0300

    refactor validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
+  FORMAT_NICKNAME = /\A\w+\z/.freeze
+
   has_secure_password
-  before_save :downcase_nickname
-  CURRENT_NICKNAME = /\A\w+\z/.freeze
-  CURRENT_EMAIL = /\A\w+@\w+\.[a-z]+\z/.freeze
+  has_many :questions, dependent: :destroy
+
+  before_validation :downcase_nickname
 
   validates :name, presence: true
-  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }
-  validates :nickname, format: { with: CURRENT_NICKNAME}
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: CURRENT_EMAIL}
-
-  has_many :questions, dependent: :destroy
+  validates :nickname, presence: true,
+            uniqueness: true,
+            length: { maximum: 40 },
+            format: { with: FORMAT_NICKNAME }
+  validates :email, presence: true,
+            uniqueness: true,
+            email: true
 
   def downcase_nickname
     nickname.downcase!
